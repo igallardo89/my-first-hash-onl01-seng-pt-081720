f5e58a78cdcb0045bd875a38ead6f34a8b738a19
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-27 01:36:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,10 +13,11 @@ def retrieval
 end
 
 def adding
-  shippinh_manifest = { 
+  shipping_manifest = { 
     "whale bone corsets" => 5,
     "porcelain vases" => 2,
     "oil paintings" => 3}
+    
     
   shipping_manifest["gun powder"] = 4
   
